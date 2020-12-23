7a9682bedf5341a53fa6c926102723c42dec8562
Eglė Juškevičienė <88egle@gmail.com>
2020-12-23 11:44:27 +0200

    form validation

--- js/components/form/Validator.js
+++ js/components/form/Validator.js
@@ -1,8 +1,17 @@
 class Validator {
     static isValidName(name) {
         // ne tuscias tekstas
+        const notEmptyString = Validator.notEmptyString(name);
+        if (notEmptyString !== true) return notEmptyString;
+
         // tik abeceles raides
-        // pirma raide didzioji
+        const onlyAlphabet = Validator.onlyAlphabet(name);
+        if (onlyAlphabet !== true) return onlyAlphabet;
+
+        // tik pirma raide didzioji
+        const onlyFirstLetterUppercase = Validator.onlyFirstLetterUppercase(name);
+        if (onlyFirstLetterUppercase !== true) return onlyFirstLetterUppercase;
+
         return true;
     }
     static isValidEmail(email) {
@@ -31,6 +40,20 @@ class Validator {
         if (text === '') {
             return 'Tekstas negali buti tuscias';
         }
+        return true;
+    }
+
+    static onlyAlphabet(text) {
+        return true;
+    }
+
+    static onlyFirstLetterUppercase(text) {
+        let convertedText = text[0].toUpperCase() + text.slice(1).toLowerCase();
+
+        if (text !== convertedText) {
+            return 'ERROR: Pirmas simbolis turi būti didžioji raidė, kitos mažosios '
+        }
+
         return true;
     }
 
